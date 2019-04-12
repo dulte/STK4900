bf5da952dbfd76a85155db2e82d8916230054055
Daniel <daniel.heinesen@sf-nett.no>
2019-04-12 11:51:58 +0200

    More done on oblig2

--- Oblig2/src/problem2.R
+++ Oblig2/src/problem2.R
@@ -1,5 +1,7 @@
 olympic=read.table("http://www.uio.no/studier/emner/matnat/math/STK4900/v17/olympic.txt",sep="\t",header=TRUE)
 
+plot(olympic)
+
 #a)
 
 
@@ -7,7 +9,7 @@ olympic=read.table("http://www.uio.no/studier/emner/matnat/math/STK4900/v17/olym
 fit.null = glm(Total2000~offset(Log.athletes),data=olympic,family=poisson)
 fit.pop = glm(Total2000~offset(Log.athletes)+Log.population,data=olympic,family=poisson)
 fit.pop.gdp = glm(Total2000~offset(Log.athletes)+Log.population + GDP.per.cap,data=olympic,family=poisson)
-fit.pop.gdp.96 = glm(Total2000~offset(Log.athletes)+Log.population + GDP.per.cap + Total1996,data=olympic,family=poisson)
+fit.pop.gdp.96 = glm(Total2000~offset(Log.athletes)+Log.population + GDP.per.cap + Total1996 ,data=olympic,family=poisson)
 
 fit.pop.96 = glm(Total2000~offset(Log.athletes)+GDP.per.cap + Total1996,data=olympic,family=poisson)
 
@@ -20,5 +22,8 @@ summary(fit.pop.gdp.96)
 anova(fit.null,fit.pop,fit.pop.gdp,fit.pop.gdp.96,test="Chisq")
 
 
-#Her virker det som Log.population of GDP.per.cap har forskjellig sigificant med of uten Total1996. Dette må diskuteres mer
-# foer jeg svarer på oppgaven.
+#Her virker det som Log.population of GDP.per.cap har forskjellig sigificant med of uten Total1996. Dette m? diskuteres mer
+# foer jeg svarer p? oppgaven.
+
+
+summary()
