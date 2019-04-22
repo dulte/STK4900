01b6a7ac562215f8192095f322c3f409c4ae7e98
Unknown <daniel.heinesen@sf-nett.no>
2019-04-22 14:15:46 +0200

    More on the report of oblig 2

--- Oblig2/src/problem2.R
+++ Oblig2/src/problem2.R
@@ -7,25 +7,19 @@ plot(olympic)
 
 #b)
 fit.null = glm(Total2000~offset(Log.athletes),data=olympic,family=poisson)
-fit.pop = glm(Total2000~offset(Log.athletes)+Log.population,data=olympic,family=poisson)
-fit.pop.gdp = glm(Total2000~offset(Log.athletes)+Log.population + GDP.per.cap,data=olympic,family=poisson)
 fit.pop.gdp.96 = glm(Total2000~offset(Log.athletes)+Log.population + GDP.per.cap + Total1996 ,data=olympic,family=poisson)
-
-fit.pop.96 = glm(Total2000~offset(Log.athletes)+GDP.per.cap + Total1996,data=olympic,family=poisson)
 fit.gdp.96 = glm(Total2000~offset(Log.athletes)+GDP.per.cap + Total1996,data=olympic,family=poisson)
 
-summary(fit.pop.96)
+summary(fit.pop.gdp.96)
+summary(fit.gdp.96)
 
+
+
+fit.pop = glm(Total2000~offset(Log.athletes)+Log.population,data=olympic,family=poisson)
+fit.pop.gdp = glm(Total2000~offset(Log.athletes)+Log.population + GDP.per.cap,data=olympic,family=poisson)
 
 summary(fit.pop)
 summary(fit.pop.gdp)
-summary(fit.pop.gdp.96)
-summary(fit.gdp.96)
-anova(fit.null,fit.pop,fit.pop.gdp,fit.pop.gdp.96,test="Chisq")
 
 
-#Her virker det som Log.population of GDP.per.cap har forskjellig sigificant med of uten Total1996. Dette m? diskuteres mer
-# foer jeg svarer p? oppgaven.
 
-
-summary()
